d2634cad77c92fb9533a2d297cb1943e3446f8f8
verafilippova <vera.yu.filippova@gmail.com>
2022-11-24 19:14:41 +0300

    add homework 22

--- 22/script.js
+++ 22/script.js
@@ -1,11 +1,11 @@
 // циклы
 
 let result = '';
-const length = 7;
+const length = 11;
 
 for(let i=1; i<length; i++) {
     for(let j =0; j<i; j++) {
-        result+='*';
+        result+='😀';
     }
     result+='\n';
 }
